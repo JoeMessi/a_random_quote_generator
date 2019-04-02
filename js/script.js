1d5087580ab02a4be9218d1669d740ca9e1bc158
Joe Messina <giuseppe91messina@gmail.com>
2019-04-02 12:24:23 +0300

    finalized and added comments

--- js/script.js
+++ js/script.js
@@ -3,17 +3,9 @@ Treehouse FSJS Techdegree:
 project 1 - A Random Quote Generator
 ******************************************/
 
-// Study guide for this project - https://drive.google.com/file/d/1s5grutGuQFwJcQP8bFwEI69Q8FCkGdDk/view?usp=sharing
-
-
-/***
-  Create the array of quote objects and name it `quotes`.
-  Add at least five quote objects to the `quotes` array.
-  Give each quote object a `quote` and `source` property.
-  Add the `citation` property to at least one object in the array.
-  Add the `year` property to at least one object in the array.
-  Use console.log() to log your array of quotes to the console.
-***/
+// This is an Array of objects. Each object is a famous quote.
+// Each quote object contains at least 2 properties: the actual quote itself and the source of the quote,
+// but there are a few more properties for some of the objects like (citation, year and category)
 
 var quotes = [
 
@@ -66,12 +58,9 @@ var quotes = [
 ];
 
 
-
-/***
-  Create the `getRandomQuote` function to:
-   - Create a variable to store a random number
-   - Cse the random number to `return` a random quote object from the `quotes` array.
-***/
+// This function return a randomly selected quote object from our Array of Object 'quotes'
+// I actually built the function with a parameter 'array' so to return a random object from the 'quotes' array
+// we need to pass 'quotes' as the argument of the getRandomQuote function when calling it.
 
 function getRandomQuote(array){
   var randomNum = Math.floor(Math.random()*10);
@@ -79,17 +68,14 @@ function getRandomQuote(array){
   return myObj;
 }
 
+
 /***
-  Create the `printQuote` function to:
-   - Call the `getRandomQuote` function and assign it to a variable.
-   - Create a variable for the HTML string and set it equal to an empty string.
-   - Use the HTML template in the instructions or the markup in the index.html file, AND
-     the random quote vairable to build your HTML string.
-   - Add the quote and source section to the HTML string.
-   - Use an if statement to check for the citation property before adding it to the HTML string.
-   - Use an if statement to check for the year property before adding it to the HTML string.
-   - Don't forget to close that final `p` tag.
-   - Set the `innerHTML` of the `quote-box` div to the HTML string.
+Inside the printQuote function the getRandomQuote function is called and its value is stored in a variable.
+Our array of objects 'quotes' is passed as the argument.
+
+From there using objects properties selection, objects methods,
+conditional statements and string concatenation we build the string
+that is going to be dispayed in the page and we set that string as the innerHTML of the div with id 'quote-box'
 ***/
 
 function printQuote() {
@@ -118,11 +104,16 @@ function printQuote() {
 }
 
 
+// This function return a random number from 0 to 255
+// ( which will be used to generate a random rgb color )
 
 function getRandomRgb() {
   return Math.floor(Math.random() * 256);
 }
 
+
+// This function calls the getRandomRgb and build and
+// return an rbg color value in the form of a string
 
 function buildColor() {
     var output = 'rgb(';
@@ -135,6 +126,9 @@ function buildColor() {
 }
 
 
+// This function changes the background of the <body> in index.html
+// with a random color everytime is called. We call the buildColor
+// function to get the rgb color value
 
 function changeBg() {
   var myBody = document.getElementsByTagName('BODY')[0];
@@ -145,17 +139,16 @@ function changeBg() {
 
 /***
   When the "Show another quote" button is clicked, the event listener
-  below will be triggered, and it will call, or "invoke", the `printQuote`
-  function. So do not make any changes to the line of code below this
-  comment.
+  below will be triggered, and it will call the `printQuote` and the
+  'changeBg' functions.
 ***/
 
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
 document.getElementById('loadQuote').addEventListener("click", changeBg, false);
 
-// maybe a better way to make those 2 into one addEventListener ???
+
+// These last 2 lines make sure that the 'printQuote' and 'changeBg' functions
+// will be called every 20 seconds
 
 window.setInterval(printQuote, 20000);
 window.setInterval(changeBg, 20000);
-
-// Remember to delete the comments that came with this file, and replace them with your own code comments.
